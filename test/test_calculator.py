67a94ff779737f9385f0325d9f16a03df5d72468
Sebastian Fernando Gomez Huanco <seabastian.gomez@uab.edu.bo>
2025-06-05 15:32:52 -0400

    test de los metodos multiplicacion y division

--- test/test_calculator.py
+++ test/test_calculator.py
@@ -1,16 +1,21 @@
 import unittest
 
-import sys
-import os
+from src.calculator import suma, resta, multiplicacion, division
 
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from src.calculator import suma, resta
+class CalculatorTest(unittest.TestCase): 
 
-class CalculatorTest(unittest.TestCase):
-   
+    #Prueba unitaria par la funcion suma
     def test_sum(self):
-        self.assertEqual(suma(2, 3), 5)
-#Prueva initaria para  la funcion suma
-   
+        self.assertEqual(suma(4, 3), 7)
+
+    #Prueba unitaria para la funcion resta
     def test_resta(self):
         self.assertEqual(resta(10, 5), 5)
+
+    #Prueba unitaria para la funcion multiplicacion
+    def test_multiplicacion(self):
+        self.assertEqual(multiplicacion(4, 3), 12)
+    
+    #Prueba unitaria para la funcion division
+    def test_division(self):
+        self.assertEqual(division(12, 4), 3)
